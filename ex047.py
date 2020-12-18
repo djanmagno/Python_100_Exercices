bc77ac762f9740457e6db617660167447140048e
djanmagno <djan.magno@gmail.com>
2020-12-17 21:29:19 -0300

    Commit to upload the exercices 11 and 29 to 50 from the Python Cursoe Video course

---
+++ ex047.py
@@ -0,0 +1,6 @@
+# All even numbers from 1 to 50
+
+for even in range(1, 51):
+    if even % 2 == 0:
+        print(even)
+print("All even number between 1 and 50 are printed above!")
---
+++ ex048.py
@@ -0,0 +1,6 @@
+# Sum of all odd numbers multiple of 3 between 1 and 500
+
+for number in range(1, 500):
+
+    if (number % 2 != 0) and (number % 3 == 0):
+        s = 
